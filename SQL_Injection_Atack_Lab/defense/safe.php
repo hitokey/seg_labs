d5084e198e2945307ff2d008005b6c0c2b70cfcd
hitokey <pedro_like07@hotmail.com>
2021-08-04 20:07:14 -0300

    Lab1

---
+++ SQL_Injection_Atack_Lab/defense/safe.php
@@ -0,0 +1,38 @@
+<?php
+// Function to create a sql connection.
+function getDB() {
+  $dbhost="10.9.0.6";
+  $dbuser="seed";
+  $dbpass="dees";
+  $dbname="sqllab_users";
+
+  // Create a DB connection
+  $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error . "\n");
+  }
+  return $conn;
+}
+
+$input_uname = $_GET['username'];
+$input_pwd = $_GET['Password'];
+$hashed_pwd = sha1($input_pwd);
+
+//printf("%s,%s,%s \n",$input_uname,$hashed_pwd,$input_pwd);
+
+// create a connection
+$conn = getDB();
+
+// do the query
+
+$text = "SELECT id, name, eid, salary, ssn FROM credential WHERE name = ? and Password = ?";
+
+$stmt = $conn->prepare($text);
+$stmt->bind_param("ss",$input_uname,$hashed_pwd);
+$stmt->execute();
+$stmt->bind_result($id,$name,$eid,$salary,$ssn);
+$stmt->fetch();
+$stmt->close();
+
+$conn->close();
+?>
